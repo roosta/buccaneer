c9c4d7775503448233ff9007e2d29da7ae7701b9
Daniel Berg <mail@roosta.sh>
2018-03-14 18:28:42 +0100

    Change company name

--- src/scrappy/core.cljs
+++ src/scrappy/core.cljs
@@ -16,7 +16,7 @@
 (def app (.-app Electron))
 
 (defn -main []
-  (.start crash-reporter (clj->js {:companyName "Your Company Name"
+  (.start crash-reporter (clj->js {:companyName "Monoxie"
                                    :submitURL   "http://example.com/"}))
 
   ;; error listener
