8ada248019bd4f49e932f43f584a526f91d7fea7
Daniel Berg <mail@roosta.sh>
2018-03-14 20:42:00 +0100

    Enable re-frisk

--- src_front_profile/scrappy_front/dev/init.cljs
+++ src_front_profile/scrappy_front/dev/init.cljs
@@ -2,11 +2,14 @@
   (:require [figwheel.client :as fw :include-macros true]
             [scrappy-front.core :as core]
             [devtools.core :as devtools]
+            [re-frisk.core :refer [enable-re-frisk!]]
             [scrappy-front.conf :as conf]))
 
 (enable-console-print!)
 
 (devtools/install!)
+
+(enable-re-frisk!)
 
 (fw/watch-and-reload
  :websocket-url   "ws://localhost:3449/figwheel-ws"
