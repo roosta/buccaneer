bda2a8b830eaf19c821265caadd663a5e161c607
Daniel Berg <mail@roosta.sh>
2018-03-17 14:15:41 +0100

    Upd project meta

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject scrappy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "View media meta data"
+  :url "https://github.com/roosta/scrappy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
