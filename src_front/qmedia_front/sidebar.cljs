dd5256afe91967c245cf4830d9ece0c7054b6221
Daniel Berg <mail@roosta.sh>
2018-09-15 19:56:42 +0200

    Add group item

--- src_front/qmedia_front/sidebar.cljs
+++ src_front/qmedia_front/sidebar.cljs
@@ -11,27 +11,64 @@
 (defgroup sidebar-style
   {:container
    {:background-color "#333"
+    :overflow-y "auto"
     :color "#eee"
-    :height "100vh"}})
+    :height "100vh"}
+
+   })
+
+(defgroup group-item-style
+  {:title {:position "relative"
+           :display "flex"
+           :align-items "center"}
+   :container {:border-top "1px solid rgba(34,36,38,.1)"}
+   :nested-item {:padding-left "24px !important"}
+   :collapsing-container {:height 0
+                          :overflow "hidden"}})
+
+(defn collapse
+  [open?]
+  (let [base {:max-height "0px"
+              :overflow "hidden"
+              :border "none"}]
+    (if open?
+      (merge base {:max-height "auto"
+                   :border-top "1px solid rgba(34,36,38,.1)"})
+      base)))
+
+(defn group-item
+  [title]
+  (let [open? (r/atom false)
+        obj @(rf/subscribe [:media/map title])]
+    (fn []
+      [:div {:class (<class group-item-style :container)}
+        [sa/MenuItem {:class (<class group-item-style :title)
+                      :on-click #(swap! open? not)}
+         title
+         [sa/Icon {:style {:color "black"
+                           :position "absolute"
+                           :right "0"}
+                   :name (if @open? "caret down" "caret right")}]]
+       [:div {:class (<class collapse @open?)}
+        (for [o obj]
+          (let [sub-title (str (:title o) " - S" (:season o) "E" (:episode o))]
+            ^{:key sub-title}
+            [sa/MenuItem {:class (<class group-item-style :nested-item)}
+             sub-title]))]]))
+  )
 
 (defn sidebar
   []
   (let [titles @(rf/subscribe [:media/titles])]
     [sa/Menu {:vertical true
+              :class (<class sidebar-style :container)
               :fluid true}
      (doall
       (for [title (sort titles)]
         (let [obj @(rf/subscribe [:media/map title])]
           (if (> (count obj) 1)
             ^{:key title}
-            [sa/Dropdown {:item true
-                          :text title}
-             [sa/DropdownMenu
-              (for [o obj]
-                (let [ext-title (str (:title o) " - S" (:season o) "E" (:episode o))]
-                  ^{:key ext-title}
-                  [sa/DropdownItem {:text ext-title}]))]]
-
+            [group-item title]
             ^{:key title}
             [sa/MenuItem {:name title}]))
         ))]))
