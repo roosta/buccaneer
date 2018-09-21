f34f7c4c9d358722e418d82005533fb2e419f49a
Daniel Berg <mail@roosta.sh>
2018-09-21 21:13:34 +0200

    Add search query for movies

--- src_front/qmedia_front/events.cljs
+++ src_front/qmedia_front/events.cljs
@@ -1,7 +1,10 @@
 (ns qmedia-front.events
+  (:require-macros [qmedia-front.macros :refer [env]])
   (:require [qmedia-front.db :as db]
-            ;; [ajax.core :as ajax]
+            [ajax.core :refer [GET POST]]
             [reagent.debug :refer [log error]]
+            [clojure.walk :refer [keywordize-keys]]
+            [debux.cs.core :as d :refer-macros [clog clogn dbg dbgn break]]
             [qmedia-front.fs :as fs]
             [clojure.string :as str]
             [cljs.nodejs :as nodejs]
@@ -16,7 +19,29 @@
 
 (def ptn (nodejs/require "parse-torrent-name"))
 
-(reg-fx :media fs/effect)
+(def search-url "https://api.themoviedb.org/3/search/movie")
+
+(reg-fx ::media fs/effect)
+
+
+(reg-event-db
+ ::save
+ (fn [db [_ query-result]]
+   (let [m (keywordize-keys query-result)])
+
+   db
+   )
+ )
+
+(reg-fx
+ ::search-movie
+ (fn [{:keys [title year]
+       :or {year false}}]
+   (GET search-url {:params {:api_key (env :api-key)
+                             :query title
+                             :year year}
+                    :response-format :json
+                    :handler #(rf/dispatch [::save %])})))
 
 (reg-event-db
  ::set-media
@@ -43,12 +68,18 @@
  :initialize-db
  (fn []
    {:db db/default-db
-    :media {:dir (:root-dir db/default-db)
+    ::media {:dir (:root-dir db/default-db)
             :on-success [::set-media]
             :on-failure [::set-error]}}))
 
-(reg-event-db
+(reg-event-fx
  :set-active-title
- (fn [db [_ obj]]
-   (assoc db :active-title obj))
+ (fn [{:keys [db]} [_ obj]]
+   (let [m {:db (assoc db :active-title obj)}]
+     (cond
+       (= (count obj) 1)
+       (let [{:keys [year title]} (first obj)]
+         (assoc m ::search-movie {:title title
+                                  :year year}))
+       :else m)))
  )
