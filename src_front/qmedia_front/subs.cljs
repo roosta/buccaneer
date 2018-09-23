aec70b066ea7f9bf8f346e6f9b6b7b96557001d6
Daniel Berg <mail@roosta.sh>
2018-09-23 15:14:32 +0200

    Add error message on wrong image size

--- src_front/qmedia_front/subs.cljs
+++ src_front/qmedia_front/subs.cljs
@@ -2,6 +2,7 @@
   (:require
    [clojure.string :as str]
    [debux.cs.core :as d :refer-macros [clog clogn dbg dbgn break]]
+   [reagent.debug :refer [log error]]
    [re-frame.core :refer [dispatch reg-event-db reg-sub reg-event-fx]]))
 
 
@@ -42,21 +43,26 @@
  :<- [:media/active]
  :<- [:themoviedb/config]
  (fn [[data config] [_ width]]
-   (let [base-url (-> config :images :base_url)
-         path (-> data :search-result :poster_path)
-         sizes (into #{} (-> config :images :poster_sizes))
-         size (get sizes width)]
-     (when (and base-url path size)
-       (str base-url size path)))))
+   (when (and (:search-result data) config)
+     (let [base-url (-> config :images :base_url)
+           path (-> data :search-result :poster_path)
+           sizes (into #{} (-> config :images :poster_sizes))
+           size (get sizes width)]
+       (if size
+         (str base-url size path)
+         (error "Unsupported backdrop width, pick one of these: " (str/join ", " sizes)))
+       ))))
 
 (reg-sub
  :media.active/backdrop-url
  :<- [:media/active]
  :<- [:themoviedb/config]
  (fn [[data config] [_ width]]
-   (let [base-url (-> config :images :base_url)
-         path (-> data :search-result :backdrop_path)
-         sizes (into #{} (-> config :images :backdrop_sizes))
-         size (get sizes width)]
-     (when (and base-url path size)
-       (str base-url size path)))))
+   (when (and (:search-result data) config)
+     (let [base-url (-> config :images :base_url)
+           path (-> data :search-result :backdrop_path)
+           sizes (into #{} (-> config :images :backdrop_sizes))
+           size (get sizes width)]
+       (if size
+         (str base-url size path)
+         (error "Unsupported backdrop width, pick one of these: " (str/join ", " sizes)))))))
