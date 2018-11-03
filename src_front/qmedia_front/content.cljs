d0d970d073ddd65d7da0611c76e91fd0612fdce7
Daniel Berg <mail@roosta.sh>
2018-11-03 19:07:48 +0100

    Add imdb icon

--- src_front/qmedia_front/content.cljs
+++ src_front/qmedia_front/content.cljs
@@ -1,6 +1,7 @@
 (ns qmedia-front.content
   (:require  [reagent.core :as r]
              [garden.units :refer [px percent]]
+             [qmedia-front.icons :as icons]
              [herb.core :refer-macros [<class defgroup]]
              [tincture.core :as t]
              [debux.cs.core :as d :refer-macros [clog clogn dbg dbgn break]]
@@ -20,19 +21,31 @@
 (defgroup root-style
   {:grid {:color (text-color)
           :height "100vh"
-          :overflow-y "auto"}})
+          :overflow-y "auto"}
+   :subheading {:margin 0}})
+
+(defgroup rating-style
+  {:icon {:width (px 32)
+          :height (px 32)}})
 
 (defn title
   []
-  [typography {:variant :display2}
-   @(rf/subscribe [:media.active/title])]
+  (let [theme @(rf/subscribe [:theme])]
+    [sa/Header {:inverted (= theme :dark)
+                :text-align "center"
+                :size "huge"}
+     @(rf/subscribe [:media.active/title])])
   )
 
 (defn ratings
   []
   (let [active @(rf/subscribe [:media/active])]
     (clog active)
-    [:div "hello"]
+    [:div
+     [icons/imdb {:class (<class rating-style :icon)}]]
+    #_[typography {:align :center
+                 :class (<class root-style :subheading)
+                 :variant :subheading}]
     )
 
   )
