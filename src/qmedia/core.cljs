f43591b9c28280bf471348358af4e68381de2089
Daniel Berg <mail@roosta.sh>
2019-01-19 13:08:48 +0100

    Update company info

--- src/qmedia/core.cljs
+++ src/qmedia/core.cljs
@@ -16,8 +16,8 @@
 (def app (.-app Electron))
 
 (defn -main []
-  (.start crash-reporter (clj->js {:companyName "roosta.sh"
-                                   :submitURL   "http://example.com/"}))
+  (.start crash-reporter (clj->js {:companyName "Monoxie"
+                                   :submitURL   "http://monoxie.com/"}))
 
   ;; error listener
   (.on nodejs/process "error"
