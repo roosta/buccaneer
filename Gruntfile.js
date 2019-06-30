bfbe73a1887773b2a4a8fd35e025febd547fda05
Daniel Berg <mail@roosta.sh>
2019-06-30 22:00:06 +0200

    Upgrade javascript deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         "download-electron": {
-            version: "4.0.1",
+            version: "5.0.6",
             outputDir: "./electron",
             // if you want to download electron into project directory
             // downloadDir: ".electron-download",
